326e9307cb538086f1d810f87151a8c879aa8511
Akiyoshi Tanaka <akkunchoi@gmail.com>
2011-09-19 23:51:02 +0900

    google calendar アクセスできるようになった

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
-google.load("jquery", "1.6");
+// http://libro.tuyano.com/index3?id=95001
+//google.load("jquery", "1.6");
 google.load("gdata", "2");
 
 var service = null;
-var url = "http://www.google.com/calendar/feeds/fvijvohm91uifvd9hratehf65k@group.calendar.google.com/public/full";
+
+// == 間違い１==
+// 非公開カレンダーは ..../public/full では
+// Error: このカレンダーの一般公開権限がありません。このカレンダーのオーナーであれば、カレンダーの共有設定を変更するとカレンダーを一般公開できます。
+// と言われる
+// var url = "https://www.google.com/calendar/feeds/tpuq2b1csas8fe435qve0taab4@group.calendar.google.com/public/full";
+// == 間違い２==
+// Protocolは http ではなく https で。でなければこのように言われる
+// 401 Authorization required
+// Unsafe JavaScript attempt to access frame with URL about:blank from frame with URL http://127.0.0.1/a/google-calendar-js/. Domains, protocols and ports must match.
+//
+// private calendar
+var url = "https://www.google.com/calendar/feeds/tpuq2b1csas8fe435qve0taab4@group.calendar.google.com/private/full";
+//var url = "https://www.google.com/calendar/feeds/default/private/full";
+var privateUrl = "https://www.google.com/calendar/feeds/tpuq2b1csas8fe435qve0taab4@group.calendar.google.com/private/full";
+//var privateUrl = "https://www.google.com/calendar/feeds/default/private/full";
+
+// public calendar
+// var url = "http://www.google.com/calendar/feeds/fvijvohm91uifvd9hratehf65k@group.calendar.google.com/public/full";
 
 google.setOnLoadCallback(getMyFeed);
 
@@ -14,11 +33,13 @@ function getMyFeed(){
   query.setSingleEvents(true);
   query.setMaxResults(10);
   var start = new google.gdata.DateTime.fromIso8601("2010-01-01");
-  var end = new google.gdata.DateTime.fromIso8601("2010-12-31"); 
+  var end = new google.gdata.DateTime.fromIso8601("2012-12-31"); 
   query.setMinimumStartTime(start);
   query.setMaximumStartTime(end);
   service.getEventsFeed(query, callback, handleError);
+
 }
+
 
 function callback(result) {
   var entries = result.feed.entry;
@@ -36,3 +57,131 @@ function handleError(error) {
   document.getElementById("view").innerHTML = error;
 }
 
+
+function doLogin(){
+  var scope = "https://www.google.com/calendar/feeds/";
+  var token = google.accounts.user.login(scope);
+  token = google.accounts.user.checkLogin(scope);
+  //console.log(token);
+  //retrieveAllCalendars();
+  //createSingleEvent();
+}
+
+function doAction(){
+  var service = null;
+  var url = privateUrl;
+  service = new google.gdata.calendar.CalendarService
+    ("calendar-sample");
+
+  var scope = "https://www.google.com/calendar/feeds/";
+  token = google.accounts.user.checkLogin(scope);
+  if (token == ""){
+    alert("ログインしてないよ！");
+    return;
+  }
+  var t1 = document.getElementById("time1").value;
+  var t2 = document.getElementById("time2").value;
+  var title = document.getElementById("title").value;
+  var start = new google.gdata.DateTime.fromIso8601(t1);
+
+  var end = new google.gdata.DateTime.fromIso8601(t2); 
+
+  var entry = new google.gdata.calendar.CalendarEventEntry();
+  entry.setTitle(google.gdata.atom.Text.create(title));
+
+  var when = new google.gdata.When();
+  when.setStartTime(start);
+  when.setEndTime(end);
+  entry.addTime(when);
+
+  var func = function(result) {
+    alert("イベントを追加しました。");
+    getMyFeed();
+  }
+  service.insertEntry(url,entry,func,handleError,
+      google.gdata.calendar.CalendarEventEntry);
+}
+
+function PRINT(e){
+  console.log(e);
+}
+function retrieveAllCalendars(){
+  /* 
+   * Retrieve all calendars 
+   */
+  // Create the calendar service object
+  //var calendarService = new google.gdata.calendar.CalendarService('GoogleInc-jsguide-1.0');
+  var calendarService = new google.gdata.calendar.CalendarService
+    ("calendar-sample");
+
+  // The default "allcalendars" feed is used to retrieve a list of all 
+  // calendars (primary, secondary and subscribed) of the logged-in user
+  var feedUri = 'https://www.google.com/calendar/feeds/default/allcalendars/full';
+
+  // The callback method that will be called when getAllCalendarsFeed() returns feed data
+  var callback = function(result) {
+
+    // Obtain the array of CalendarEntry
+    var entries = result.feed.entry;
+
+    for (var i = 0; i < entries.length; i++) {
+      var calendarEntry = entries[i];
+      var calendarTitle = calendarEntry.getTitle().getText();
+      PRINT('Calendar title = ' + calendarTitle);
+    }
+  }
+
+  // Error handler to be invoked when getAllCalendarsFeed() produces an error
+  var handleError = function(error) {
+    PRINT(error);
+  }
+
+  // Submit the request using the calendar service object
+  calendarService.getAllCalendarsFeed(feedUri, callback, handleError);
+}
+
+function createSingleEvent(){
+
+  /*
+   * Create a single event
+   */ 
+
+  // Create the calendar service object
+  var calendarService = new google.gdata.calendar.CalendarService('GoogleInc-jsguide-1.0');
+
+  // The default "private/full" feed is used to insert event to the 
+  // primary calendar of the authenticated user
+  var feedUri = privateUrl
+
+  // Create an instance of CalendarEventEntry representing the new event
+  var entry = new google.gdata.calendar.CalendarEventEntry();
+
+  // Set the title of the event
+  entry.setTitle(google.gdata.atom.Text.create('JS-Client: insert event'));
+
+  // Create a When object that will be attached to the event
+  var when = new google.gdata.When();
+
+  // Set the start and end time of the When object
+  var startTime = google.gdata.DateTime.fromIso8601("2011-09-19T09:00:00.000-08:00");
+  var endTime = google.gdata.DateTime.fromIso8601("2011-09-19T10:00:00.000-08:00");
+  when.setStartTime(startTime);
+  when.setEndTime(endTime);
+
+  // Add the When object to the event 
+  entry.addTime(when);
+
+  // The callback method that will be called after a successful insertion from insertEntry()
+  var callback = function(result) {
+    PRINT('event created!');
+  }
+
+  // Error handler will be invoked if there is an error from insertEntry()
+  var handleError = function(error) {
+    PRINT(error);
+  }
+
+  // Submit the request using the calendar service object
+  calendarService.insertEntry(feedUri, entry, callback, 
+      handleError, google.gdata.calendar.CalendarEventEntry);
+}
