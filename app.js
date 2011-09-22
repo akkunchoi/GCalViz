1a4e78abb1e9a1d381428193b553e56cd4c73463
Akiyoshi Tanaka <akkunchoi@gmail.com>
2011-09-22 22:53:09 +0900

    今月データを時単位で出力

--- app.js
+++ app.js
@@ -3,7 +3,10 @@
  * ----------------
  *
  * [Done] Calendarを選択させる
- * 月間グラフを出力する
+ * [Done] 月間グラフを出力する
+ *   [Done] 今月の一覧を取得する
+ *   [Done] All day Eventを除く
+ * 分単位で出力する 
  * 月を変更する
  * pushState/breadcrumbs/back UI or something
  * Loading Interface
@@ -98,6 +101,51 @@ var GCalService = (function(){
 
       return dfd.promise();
     }
+    /**
+     * 全日イベントかどうか
+     * @param google.gdata.calendar.CalendarEventEntry event
+     * @return boolean true if event is all day event
+     */
+    var isAllDayEvent = function(event){
+      var time = event.getTimes()[0];
+      var startTime = google.gdata.DateTime.fromIso8601(time.startTime);
+      var endTime = google.gdata.DateTime.fromIso8601(time.endTime);
+      return startTime.isDateOnly() && endTime.isDateOnly();
+    }
+    /**
+     * 今月のフィードを取得
+     */
+    this.getFeeds = function(url, date){
+      query = new google.gdata.calendar.CalendarEventQuery(url);
+      query.setOrderBy(google.gdata.calendar.CalendarEventQuery.ORDERBY_START_TIME);
+      query.setSortOrder(google.gdata.calendar.CalendarEventQuery.SORTORDER_ASCENDING);
+      query.setSingleEvents(true);
+      query.setMaxResults(100); // Todo: variable
+      var start = google.gdata.DateTime.fromIso8601(google.gdata.DateTime.toIso8601(date).slice(0, 7) + '-01');
+      // Todo: 31日もない月はどうなるか確認
+      var end = google.gdata.DateTime.fromIso8601(google.gdata.DateTime.toIso8601(date).slice(0, 7) + '-31');
+      query.setMinimumStartTime(start);
+      query.setMaximumStartTime(end);
+      
+      var dfd = jQuery.Deferred();
+      var callback = function(result) {
+        var entries = result.feed.entry;
+        // 全日イベントを除外 Todo: オプション化
+        var filteredEntries = jQuery.grep(entries, function(e, i){
+          return !isAllDayEvent(e);
+        });
+        dfd.resolve(filteredEntries);
+      }
+
+      var handleError = function(error) {
+        document.getElementById("view").innerHTML = error;
+        dfd.reject(error);
+      }
+      this.getService().getEventsFeed(query, callback, handleError);
+
+      return dfd.promise();
+
+    }
 
     /**
      *
@@ -110,6 +158,7 @@ var GCalService = (function(){
       // calendars (primary, secondary and subscribed) of the logged-in user
       var feedUri = 'https://www.google.com/calendar/feeds/default/allcalendars/full';
 
+      // Deferredのこのへん抽象化できるかな？
       var dfd = jQuery.Deferred();
 
       // The callback method that will be called when getAllCalendarsFeed() returns feed data
@@ -133,6 +182,111 @@ var GCalService = (function(){
     }
   }
 })();
+
+var DateTime = (function(){
+  var aDayMilliSecond = 86400000;
+  var DateTime = function(obj){
+    var d = null;
+    var construct = function(obj){
+      if (obj instanceof Date){
+        d = obj;
+      }else if (obj == null){
+        d = new Date();
+      }else if (!isNaN(obj)){
+        d = new Date(obj);
+      }else if (obj instanceof DateTime){
+        d = obj;
+      }else{
+        throw new Exception;
+      }
+    }
+    construct(obj);
+    /**
+     * 
+     * @return Date
+     */
+    this.getDate = function(){
+      return d;
+    }
+    /**
+     * date.getFullYear()
+     * @return Number
+     */
+    this.getYear = function(){
+      return d.getFullYear();
+    }
+    /**
+     * date.getMonth() + 1
+     * @return Number
+     */
+    this.getMonth = function(){
+      return d.getMonth() + 1;
+    }
+    /**
+     * date.getDate()
+     * @return Number
+     */
+    this.getDay = function(){
+      return d.getDate();
+    }
+    /**
+     * @return DateTime
+     */
+    this.getMonthFirst = function(){
+      return new DateTime(new Date(d.getFullYear(), d.getMonth(), 1));
+    }
+    /**
+     * @return DateTime
+     */
+    this.getMonthLast = function(){
+      // 来月 - 1日
+      return new DateTime(new Date((new Date(d.getFullYear() + Math.floor(d.getMonth()/11), d.getMonth() + 1, 1)).getTime() - aDayMilliSecond));
+    }
+    /**
+     * @return DateTime
+     */
+    this.yesterday = function(){
+      return new DateTime(d.getTime() - aDayMilliSecond);
+    }
+    /**
+     * @return DateTime
+     */
+    this.tomorrow = function(){
+      return new DateTime(d.getTime() + aDayMilliSecond);
+    }
+    /**
+     * @return String
+     */
+    this.toString = function(){
+      return d.toString();
+    }
+    /**
+     * @return Number
+     */
+    this.compareTo = function(obj){
+      obj = new DateTime(obj);
+      var a = this.getDate().getTime();
+      var b = obj.getDate().getTime();
+      if (a > b){
+        return 1;
+      }else if (a < b){
+        return -1;
+      }else{
+        return 0;
+      }
+    }
+    this.afterBySecond = function(second){
+      return new DateTime(d.getTime() + second * 1000);
+    }
+  }
+  return DateTime;
+})();
+/*
+console.log(new DateTime().getYear());  
+console.log(new DateTime().getMonth());  
+console.log(new DateTime().getDay());  
+*/
+
 /**
  *
  *
@@ -148,7 +302,6 @@ var App = (function(){
      * @param Function callback function(element, jQuery)
      */ 
     var listify = function(elements, callback){
-      $('#view').empty();
       var div = $(document.createElement('ul'));
       $.each(elements, function(k, e){
         var li = $(document.createElement('li'));
@@ -158,31 +311,103 @@ var App = (function(){
       });
       $('#view').append(div);
     }
+    var t = function(tag){
+      return $(document.createElement(tag));
+    }
+    var eachDayOfMonth = function(date, callback){
+      var monthFirst = new DateTime(date).getMonthFirst();
+      var datetime = new DateTime(date).getMonthFirst();
+      do{
+        callback(datetime.getDate());
+        datetime = datetime.tomorrow();
+      }while(datetime.getMonth() == monthFirst.getMonth());
+    }
     /**
      *
-     * @param String url calendar feed url
+     * @param google.gdata.calendar.CalendarEntry calendar
      */
-    var screenRecentFeeds = function(url){
-      service.getRecentFeeds(url).then(function(res){
+    var screenRecentFeeds = function(calendar){
+      var url = calendar.getLink().getHref()
+      var field = $('#view');
+      field.empty();
+
+      var title = t('h2').appendTo(field);
+      title.text(calendar.getTitle().getText());
+      
+      var today = new Date();
+      var date = new google.gdata.DateTime(today, true);
+
+      var subtitle = t('h3').appendTo(field);
+      var yearMonthFormat = new DateFormat("yyyy/MM");
+      subtitle.text(yearMonthFormat.format(today));
+
+      // レンダリング、ピクセル単位でやった方が分単位で正確にできそう
+      // 月のタイムテーブルを出力
+      var table = t('table').appendTo(field).addClass('timetable');
+      var tbody = t('tbody').appendTo(table);
+
+      var dateId = function(date){
+        return 'tt-' + date.getFullYear() + '-' + (date.getMonth()+1) + '-' + date.getDate() + '-' + date.getHours();
+      }
+      eachDayOfMonth(today, function(day){
+        var tr = t('tr').appendTo(table);
+        var th = t('th').appendTo(tr);
+        th.text(day.getDate());
+        _(24).times(function(i){
+          var td = t('td').appendTo(tr);
+          var tdTime = new Date(day.getFullYear(), day.getMonth(), day.getDate(), i+1, 0, 0);
+          td.attr('id', dateId(tdTime));
+          td.html('&nbsp;&nbsp;&nbsp;&nbsp;');
+        });
+      });
+      
+      service.getFeeds(url, date).then(function(res){
+        // 該当日時を色付け
+        _.each(res, function(e){
+          var times = e.getTimes();
+          var startDate = google.gdata.DateTime.fromIso8601(times[0].startTime).getDate();
+          var endDate = google.gdata.DateTime.fromIso8601(times[0].endTime).getDate();
+
+          // 合計時間
+          var ms = endDate.getTime() - startDate.getTime();
+          var valHour  = Math.floor(ms / 3600 / 10) / 100;
+         
+          var d = new DateTime(startDate);
+          do{
+            var a = $('#' + dateId(d.getDate()));
+            a.addClass('filled');
+            d = d.afterBySecond(3600);
+            console.log(d);
+          }while(d.compareTo(endDate) <= 0);
+
+        });
+        /*
         listify(res, function(e, button){
           var title = e.getTitle().getText();
           var times = e.getTimes();
-          button.text(title + " " + times[0].startTime);
+          var startDate = google.gdata.DateTime.fromIso8601(times[0].startTime).getDate();
+          var endDate = google.gdata.DateTime.fromIso8601(times[0].endTime).getDate();
+          var ms = endDate.getTime() - startDate.getTime();
+          // 合計時間
+          var valHour  = Math.floor(ms / 3600 / 10) / 100;
+          var dateFormat = new DateFormat("yyyy/MM/dd HH:mm:ss");
+          button.text(title + " " + dateFormat.format(startDate) + " " + dateFormat.format(endDate) + " " + valHour);
         });
+        */
       });
     }
     /**
      *
      */
     var screenAllCalendars = function(){
+      $('#view').empty();
       service.retrieveAllCalendars().then(function(res){
         listify(res, function(e, button){
           var title = e.getTitle().getText();
           button.text(title);
           button.attr('href', '#');
           button.click(function(){
-            var href = e.getLink().getHref()
-            screenRecentFeeds(href);
+            screenRecentFeeds(e);
             return false;
           });
         });
